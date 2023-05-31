1e34cbb6167f72ebd0661fec5209aedfa79e1b1e
Jairo Garciga <jairo@statsig.com>
2023-05-31 12:04:27 -0700

    Add Action, Typescript files, and some jsons

--- ProjectData.ts
+++
@@ -1,7 +0,0 @@
-import axios, { AxiosError, AxiosResponse } from "axios";
-import axiosRetry from 'axios-retry';
-
-axiosRetry(axios, {
-    retries: 3,
-});
-
---
+++ src/ProjectData.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+import axiosRetry from 'axios-retry';
+
+axiosRetry(axios, {
+    retries: 3,
+});
+
+// Calls the endpoint using the API key and gets the projects info
+export default async function getProjectData() {
+
+    let projectRes: AxiosResponse | undefined;
+
+    try {
+        projectRes = await axios.post(
+            'https://statsigapi.net/developer/v1/projects',
+            null,
+            {
+                headers: {
+                    "statsig-api-key": "6wdiBivL3kECj1ducAZrc4:Ie1nOKs9KVAkCOwPnPiiUjCdipPPXAW0yVZNvHFQq6h",
+                    //"statsig-api-key": process.env.STATSIG_API_KEY,
+                    'Content-Type': 'application/json',
+                },
+                timeout: 10000,
+            }
+        )
+    } catch (e: unknown) {
+        projectRes = (e as AxiosError)?.response;
+        console.log("Error Requesting after 4 attempts")
+    }
+
+    const data = projectRes?.data;
+    return data;
+}
