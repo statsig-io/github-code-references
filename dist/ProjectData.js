2912eb98a08ba2025a51f931260150b23efd9d4d
Jairo Garciga <jairo@statsig.com>
2023-05-31 13:21:28 -0700

    Update tsconfig to use es2020

--- dist/ProjectData.js
+++ dist/ProjectData.js
@@ -1,76 +1,29 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var axios_1 = require("axios");
-var axios_retry_1 = require("axios-retry");
+const axios_1 = require("axios");
+const axios_retry_1 = require("axios-retry");
 (0, axios_retry_1.default)(axios_1.default, {
     retries: 3,
 });
 // Calls the endpoint using the API key and gets the projects info
-function getProjectData() {
-    return __awaiter(this, void 0, void 0, function () {
-        var projectRes, e_1, data;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    _a.trys.push([0, 2, , 3]);
-                    return [4 /*yield*/, axios_1.default.post('https://statsigapi.net/developer/v1/projects', null, {
-                            headers: {
-                                "statsig-api-key": "6wdiBivL3kECj1ducAZrc4:Ie1nOKs9KVAkCOwPnPiiUjCdipPPXAW0yVZNvHFQq6h",
-                                //"statsig-api-key": process.env.STATSIG_API_KEY,
-                                'Content-Type': 'application/json',
-                            },
-                            timeout: 10000,
-                        })];
-                case 1:
-                    projectRes = _a.sent();
-                    return [3 /*break*/, 3];
-                case 2:
-                    e_1 = _a.sent();
-                    projectRes = e_1 === null || e_1 === void 0 ? void 0 : e_1.response;
-                    console.log("Error Requesting after 4 attempts");
-                    return [3 /*break*/, 3];
-                case 3:
-                    data = projectRes === null || projectRes === void 0 ? void 0 : projectRes.data;
-                    return [2 /*return*/, data];
-            }
+async function getProjectData() {
+    let projectRes;
+    try {
+        projectRes = await axios_1.default.post('https://statsigapi.net/developer/v1/projects', null, {
+            headers: {
+                "statsig-api-key": "6wdiBivL3kECj1ducAZrc4:Ie1nOKs9KVAkCOwPnPiiUjCdipPPXAW0yVZNvHFQq6h",
+                //"statsig-api-key": process.env.STATSIG_API_KEY,
+                'Content-Type': 'application/json',
+            },
+            timeout: 10000,
         });
-    });
+    }
+    catch (e) {
+        projectRes = e?.response;
+        console.log("Error Requesting after 4 attempts");
+    }
+    const data = projectRes?.data;
+    return data;
 }
 exports.default = getProjectData;
 //# sourceMappingURL=ProjectData.js.map
