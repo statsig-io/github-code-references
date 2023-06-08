7e4cead7dfce95863565fefde718b19216c6fbab
Jairo Garciga <jairo@statsig.com>
2023-06-08 14:24:47 -0700

    Quick commit because I stayed on the branch

---
+++ tests/stale_gates.ts
@@ -0,0 +1,17 @@
+import statsig from 'statsig-node';
+import Utils from '../src/Utils'
+
+
+// Test regex matching and cleaning up these gates
+async function useDummyGates() {
+    // Put a gate here so we can search for it later
+
+    const dummyUser = {
+        userID: '12345',
+        email: '12345@gmail.com',
+    };
+
+    const dummyGate = await statsig.checkGate(dummyUser, 'dummy_gate');
+    const sillyGate = await statsig.checkGate(dummyUser, 'silly_gate');
+    const typescriptDC = await statsig.getConfig(dummyUser, 'typescript_dynamic_config');
+}
