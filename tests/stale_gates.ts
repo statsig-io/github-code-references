786f2154c9f40f287d4496c2f3e25767452afd6b
Jairo Garciga <jairo@statsig.com>
2023-06-09 11:15:47 -0700

    test

--- tests/stale_gates.ts
+++ tests/stale_gates.ts
@@ -11,5 +11,8 @@ async function useDummyGates() {
 
     const dummyGate = await statsig.checkGate(dummyUser, 'dummy_gate');
     const sillyGate = await statsig.checkGate(dummyUser, 'silly_gate');
+    const hmm = await statsig.checkGate(dummyUser, 'dummy_gate');
+    const uhh = await statsig.checkGate(dummyUser, 'silly_gate');
+
     const typescriptDC = await statsig.getConfig(dummyUser, 'typescript_dynamic_config');
 }
