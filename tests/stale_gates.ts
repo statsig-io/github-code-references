100359dcf5018ef40136c5ca8cb71a4f8e82ad8c
Jairo Garciga <jairo@statsig.com>
2023-06-09 10:37:35 -0700

    Fix cleaned gates

--- tests/stale_gates.ts
+++ tests/stale_gates.ts
@@ -1,8 +1,6 @@
 import statsig from 'statsig-node';
 import Utils from '../src/Utils'
 
-
-// Test regex matching and cleaning up these gates
 async function useDummyGates() {
     // Put a gate here so we can search for it later
 
@@ -11,7 +9,7 @@ async function useDummyGates() {
         email: '12345@gmail.com',
     };
 
-    const dummyGate = false;
-    const sillyGate = false;
+    const dummyGate = await statsig.checkGate(dummyUser, 'dummy_gate');
+    const sillyGate = await statsig.checkGate(dummyUser, 'silly_gate');
     const typescriptDC = await statsig.getConfig(dummyUser, 'typescript_dynamic_config');
 }
