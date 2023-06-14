d2d2753da60074230d5263050f58675da9c9b1f8
Jairo Garciga <jairo@statsig.com>
2023-06-14 10:42:42 -0700

    Test cleaning gates and pr creation

--- src/ProjectData.ts
+++ src/ProjectData.ts
@@ -64,7 +64,8 @@ export default async function getProjectData() {
                 updatedGates.push(gate) 
 
                 // Create the map
-                if (isGateStale(gate.gateType.reason)) {
+                // if (isGateStale(gate.gateType.reason)) { Test on Temporary gates
+                if (gate.gateType.reason == "TEMPORARY") {
                     const fileDir = fileWithGates.fileDir;
 
                     if (staleGates.has(fileDir)) {
