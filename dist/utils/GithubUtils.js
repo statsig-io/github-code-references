740f94bbb11045d3bb96c44ce661da766ab7a4c5
jairo-statsig <jairo@statsig.com>
2023-06-22 11:05:12 -0700

    backup stashing

--- dist/utils/GithubUtils.js
+++ dist/utils/GithubUtils.js
@@ -83,6 +83,7 @@ class GithubUtils {
     async setupBranchLocally(targetBranch) {
         // Checkout the branch!
         await this.git.fetch();
+        await this.git.stash(); // Just in case
         await this.git.checkout(targetBranch, ['-f']); // Force the swap, ignore local changes
         const branch = await this.git.branch();
         const currentBranch = branch.current;
