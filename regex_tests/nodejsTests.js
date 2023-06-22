4b6b25df490ac9859a74947c3ccb0762af67e363
Statsig-GithubCodeRefs <githubcoderefs@statsig.com>
2023-06-22 17:38:05 +0000

    Clean stale gates and configs

--- regex_tests/nodejsTests.js
+++ regex_tests/nodejsTests.js
@@ -5,6 +5,6 @@ const dummyUser = {
     email: '12345@gmail.com',
 };
 
-await statsig.checkGate(dummyUser, 'silly_gate')
-const thirdDummy = await statsig.checkGate(dummyUser, 'node_js_gate')
+
+const thirdDummy = false;
 const configTest = await statsig.getConfig(dummyUser, 'nodejs_dynamic_config');
